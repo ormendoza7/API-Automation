633af95c70678dfe23a2c72bb44b0db4efbb9b83
Orlando Mendoza Mamani <Orlando.Mendoza@jalasoft.com>
2024-03-18 22:56:37 -0400

    Task 2 updated

--- asana_api/conftest.py
+++ asana_api/conftest.py
@@ -1,30 +1,32 @@
 import pytest
 import requests
 import logging
-from config.config import ASANA_API_URL, CONTENT_HEADERS, COMMON_HEADERS, WORKSPACE_ID, TEST_PROJECT_NAME
+from config.config import CONTENT_HEADERS, COMMON_HEADERS, TEST_PROJECT_NAME, CREATE_PROJECT_URL, PROJECT_URL
 from utils.logger import get_logger
 
+LOGGER = get_logger(__name__, logging.INFO)
 
-LOGGER = get_logger(__name__, logging.DEBUG)
+
 @pytest.fixture()
+#@pytest.fixture(scope="module")
 def project_created():
     # Create Project
-    LOGGER.debug("Create project fixture")
+    LOGGER.info("Starting project creation fixture.")
 
-    url = f'{ASANA_API_URL}/workspaces/{WORKSPACE_ID}/projects'
     data = {'data': {'name': TEST_PROJECT_NAME}}
 
-    response = requests.post(url, headers=CONTENT_HEADERS, json=data)
-    project = response.json()['data']
+    response = requests.post(CREATE_PROJECT_URL, headers=CONTENT_HEADERS, json=data)
     assert response.status_code == 201
+
+    project = response.json()['data']
     assert TEST_PROJECT_NAME == project['name']
 
+    LOGGER.info(f"Project created with ID: {project['gid']} and name: {TEST_PROJECT_NAME}")
     yield project
 
     # Delete Project
-    LOGGER.info("Cleanup project...")
+    LOGGER.info(f"Cleaning up project with ID: {project['gid']}.")
     project_id = project['gid']
-    url = f'{ASANA_API_URL}/projects/{project_id}'
-    response = requests.delete(url, headers=COMMON_HEADERS)
+    response = requests.delete(PROJECT_URL(project_id), headers=COMMON_HEADERS)
     assert response.status_code == 200
-    LOGGER.info("Project Id: %s deleted", project_id)
+    LOGGER.info(f"Project with ID: {project['gid']} deleted successfully.")
